d6122a1271864748dac7879c82cb3e60e2208a79
Dustin Black <dblack@redhat.com>
2022-08-22 09:19:57 +0200

    add step to stop operator

--- python/benchmark-operator/benchmark-operator_plugin.py
+++ python/benchmark-operator/benchmark-operator_plugin.py
@@ -15,11 +15,13 @@ class InputParams:
     """
     This is the data structure for the input parameters of the step defined below.
     """
-    name: typing.Annotated[str, validation.min(1)] = field(metadata={
-        "name": "Name",
-        "description": "Enter your name here."
-    })
+    #name: typing.Annotated[str, validation.min(1)] = field(metadata={
+    #    "name": "Name",
+    #    "description": "Enter your name here."
+    #})
     kubeconfig: str
+    #operator_repo: str = "https://github.com/cloud-bulldozer/benchmark-operator.git"
+    #operator_branch: str = "v1.0.1"
 
 
 @dataclass
@@ -41,12 +43,12 @@ class ErrorOutput:
 # The following is a decorator (starting with @). We add this in front of our function to define the metadata for our
 # step.
 @plugin.step(
-    id="start-benchmark-operator",
+    id="start",
     name="Start Benchmark Operator",
     description="Deploys the Benchmark Operator",
     outputs={"success": SuccessOutput, "error": ErrorOutput},
 )
-def benchmark_operator(params: InputParams) -> typing.Tuple[str, typing.Union[SuccessOutput, ErrorOutput]]:
+def start_benchmark_operator(params: InputParams) -> typing.Tuple[str, typing.Union[SuccessOutput, ErrorOutput]]:
     """
     The function  is the implementation for the step. It needs the decorator above to make it into a  step. The type
     hints for the params are required.
@@ -55,27 +57,25 @@ def benchmark_operator(params: InputParams) -> typing.Tuple[str, typing.Union[Su
 
     :return: the string identifying which output it is, as well the output structure
     """
-    
-    operator_repo = "https://github.com/cloud-bulldozer/benchmark-operator.git"
-    operator_branch = "v1.0.1"
+
     kubeconfig_file = tempfile.mkstemp()
 
     print("==>> Importing kubeconfig...")
     with open(kubeconfig_file[1], 'w') as file:
         file.write(params.kubeconfig)
 
-    kubectl_cmd = [
-        "kubectl",
-        "--kubeconfig={}".format(kubeconfig_file[1]),
-        "version",
-        "--short"
-    ]
-
-    try:
-        print(subprocess.check_output(kubectl_cmd, text=True, stderr=subprocess.STDOUT))
-    except subprocess.CalledProcessError as error:
-        return "error", ErrorOutput("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
-
+#    #debug
+#    kubectl_cmd = [
+#        "kubectl",
+#        "--kubeconfig={}".format(kubeconfig_file[1]),
+#        "version",
+#        "--short"
+#    ]
+#
+#    try:
+#        print(subprocess.check_output(kubectl_cmd, text=True, stderr=subprocess.STDOUT))
+#    except subprocess.CalledProcessError as error:
+#        return "error", ErrorOutput("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
 
     ripsaw_cmd = [
         "make",
@@ -90,11 +90,44 @@ def benchmark_operator(params: InputParams) -> typing.Tuple[str, typing.Union[Su
         return "error", ErrorOutput("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
 
     print("==>> Benchmark Operator Deployment Complete!")
-    return "success", SuccessOutput(
-            "Success, {}!".format(params.name))
+    return "success", SuccessOutput("Benchmark Operator successfully deployed!")
+
+
+# The following is a decorator (starting with @). We add this in front of our function to define the metadata for our
+# step.
+@plugin.step(
+    id="stop",
+    name="Stop Benchmark Operator",
+    description="Undeploys the Benchmark Operator",
+    outputs={"success": SuccessOutput, "error": ErrorOutput},
+)
+def stop_benchmark_operator(params: InputParams) -> typing.Tuple[str, typing.Union[SuccessOutput, ErrorOutput]]:
+
+    kubeconfig_file = tempfile.mkstemp()
+
+    print("==>> Importing kubeconfig...")
+    with open(kubeconfig_file[1], 'w') as file:
+        file.write(params.kubeconfig)
+
+    ripsaw_cmd = [
+        "make",
+        "undeploy"
+    ]
+
+    print("==>> Stopping benchmark-operator...")
+    try:
+        print(subprocess.check_output(ripsaw_cmd, cwd="benchmark-operator", env={"KUBECONFIG": kubeconfig_file[1]},
+            text=True, stderr=subprocess.STDOUT))
+    except subprocess.CalledProcessError as error:
+        return "error", ErrorOutput("{} failed with return code {}:\n{}".format(error.cmd[0], error.returncode, error.output))
+
+    print("==>> Benchmark Operator Stopped!")
+    return "success", SuccessOutput("Benchmark Operator successfully removed!")
+
 
 if __name__ == "__main__":
     sys.exit(plugin.run(plugin.build_schema(
         # List your step functions here:
-        benchmark_operator,
+        start_benchmark_operator,
+        stop_benchmark_operator,
     )))
